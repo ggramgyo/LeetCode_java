6928bbe46279a659b11bc9f51c48de2046cf5184
ggramgyo <88009952+ggramgyo@users.noreply.github.com>
2024-07-09 20:39:15 +0900

    Time: 2 ms (27.43%), Space: 41.6 MB (30.86%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -0,0 +1,76 @@
+class Solution {
+    class TrieNode {
+        TrieNode[] children;
+        boolean isEndOfWord;
+
+        TrieNode() {
+            children = new TrieNode[26];
+            isEndOfWord = false;
+        }
+    }
+
+    class Trie {
+        TrieNode root;
+
+        Trie() {
+            root = new TrieNode();
+        }
+
+        void insert(String word) {
+            TrieNode node = root;
+            for (char c : word.toCharArray()) {
+                int idx = c - 'a';
+                if (node.children[idx] == null) {
+                    node.children[idx] = new TrieNode();
+                }
+                node = node.children[idx];
+            }
+            node.isEndOfWord = true;
+        }
+
+        String longestPrefix() {
+            TrieNode node = root;
+            StringBuilder sb = new StringBuilder();
+            while (node != null) {
+                TrieNode next = null;
+                int cnt = 0;
+                for (int i = 0; i < 26; i++) {
+                    if (node.children[i] != null) {
+                        cnt++;
+                        next = node.children[i];
+                    }
+                }
+                if (cnt == 1 && !node.isEndOfWord) {
+                    sb.append((char) ('a' + getIndex(next, node.children)));
+                    node = next;
+                } else {
+                    break;
+                }
+            }
+            return sb.toString();
+        }
+
+        private int getIndex(TrieNode node, TrieNode[] children) {
+            for (int i = 0; i < children.length; i++) {
+                if (children[i] == node) {
+                    return i;
+                }
+            }
+            return -1;
+        }
+    }
+
+    public String longestCommonPrefix(String[] strs) {
+        if (strs == null || strs.length == 0) {
+            return "";
+        }
+
+        Trie trie = new Trie();
+        for (String str : strs) {
+            trie.insert(str);
+        }
+        return trie.longestPrefix();
+    }
+
+
+}
